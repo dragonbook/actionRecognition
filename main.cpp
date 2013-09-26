92643b0880120357eb106255f1ab73ffe54249e9
dragonbook <573260700@qq.com>
2013-09-26 14:14:28 +0800

    removed image background using skeleton joints depth range

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "dataViewer/ReadActivity.h"
 #include <opencv2/imgproc/imgproc.hpp>
 
+//
+#define JOINT_NUM_2D 15
+
 //////////////////////////////////////////////////////////////////////////
 
 // images and skeleton
@@ -18,8 +21,21 @@ ReadActivity activity;
 //////////////////////////////////////////////////////////////////////////
 
 
+//////////////////////////////////////////////////////////////////////////
+
+// skeleton joints in 2D image
+
+int joints2D[JOINT_NUM_2D][3];
+
+//////////////////////////////////////////////////////////////////////////
+
+
 // allocate resources for data
 void initDatas();
+
+void calculateJoints2D();
+
+void getBodyBoundingBox(int& minX, int& maxX, int& minY, int& maxY, int& minZ, int& maxZ);
 
 void showActivity(const string& activityId, const string& dataPathRGBD, const string& dataPathSkeleton);
 
@@ -43,22 +59,101 @@ void showActivity(const string& activityId, const string& dataPathRGBD, const st
 
 	initDatas();
 
-	cv::namedWindow("RGB_Window", CV_WINDOW_AUTOSIZE);
-	cv::namedWindow("Depth_Window", CV_WINDOW_AUTOSIZE);
+//	cv::namedWindow("RGB_Window", CV_WINDOW_AUTOSIZE);
+//	cv::namedWindow("Depth_Window", CV_WINDOW_AUTOSIZE);
 	cv::namedWindow("Gray_Window", CV_WINDOW_AUTOSIZE);
+
+	cv::Mat mask;
+	cv::namedWindow("Mask_Window", CV_WINDOW_AUTOSIZE);
 
 
 	while (activity.readNextFrame(data, posData, dataCONF, posDataCONF, imgRGB, imgDepth)) {
 		cv::imshow("RGB_Window", imgRGB);
-		cv::imshow("Depth_Window", imgDepth);
+//		cv::imshow("Depth_Window", imgDepth);
 
 		// RGB to gray
 		cvtColor(imgRGB, imgGray, CV_RGB2GRAY, 1);
-		cv::imshow("Gray_Window", imgGray);
+//		cv::imshow("Gray_Window", imgGray);
 
-		cv::waitKey(10);
+		//
+		calculateJoints2D();
+
+		// Get mask from depth image and skeleton
+		int minX, maxX, minY, maxY, minZ, maxZ;
+
+		getBodyBoundingBox(minX, maxX, minY, maxY, minZ, maxZ);
+
+		/*
+		minX -= 20;
+		minY -= 20;
+		maxX += 20;
+		maxY += 20;
+		*/
+
+		printf("%d, %d\n", minZ, maxZ);
+
+//		mask.create(imgDepth.size(), imgDepth.type());
+		mask = imgDepth.clone();
+
+		// set pixel value to zero if it is greater than max or lower than min
+		for (int i = 0; i < mask.rows; i++) {
+			uchar* data = mask.ptr<uchar>(i);
+
+			for (int j = 0; j < mask.cols; j++) { 
+				if (data[j] < minZ || data[j] > maxZ) {
+					data[j] = 0;
+				}
+				else {
+					data[j] = 255;
+				}
+
+				if (i >= minY && i <= maxY && j >= minX && j <= maxX) {
+					//
+				}
+				else {
+//					data[j] = 0;
+				}
+			}
+		}
+
+		cv::imshow("Mask_Window", mask);
+
+//		cv::waitKey(10);
+		cv::waitKey();
 	}
 
+}
+
+void calculateJoints2D()
+{
+	for (int i = 0; i < JOINT_NUM; i++) {
+		joints2D[i][0] = 2 * (int)(156.8584456124928 + 0.0976862095248 * data[i][9] - 0.0006444357104 * data[i][10] + 0.0015715946682 * data[i][11]);
+		joints2D[i][1] = 2 * (int)(125.5357201011431 + 0.0002153447766 * data[i][9] - 0.1184874093530 * data[i][10] - 0.0022134485957 * data[i][11]);
+		joints2D[i][2] = (imgDepth.ptr<uchar>(joints2D[i][1]))[joints2D[i][0]];
+	}
+
+	for (int i = JOINT_NUM; i < JOINT_NUM + POS_JOINT_NUM; i++) {
+		joints2D[i][0] = 2 * (int)(156.8584456124928 + 0.0976862095248 * posData[i - JOINT_NUM][0] - 0.0006444357104 * posData[i - JOINT_NUM][1] + 0.0015715946682 * posData[i - JOINT_NUM][2]);
+		joints2D[i][1] = 2 * (int)(125.5357201011431 + 0.0002153447766 * posData[i - JOINT_NUM][0] - 0.1184874093530 * posData[i - JOINT_NUM][1] - 0.0022134485957 * posData[i - JOINT_NUM][2]);
+		joints2D[i][2] = (imgDepth.ptr<uchar>(joints2D[i][1]))[joints2D[i][0]];
+	}
+}
+
+void getBodyBoundingBox(int& minX, int& maxX, int& minY, int& maxY, int& minZ, int& maxZ)
+{
+	minX = minY = minZ = 1000;
+	maxX = maxY = maxZ = 0;
+
+	for (int i = 0; i < JOINT_NUM_2D; i++) {
+		if (joints2D[i][0] < minX) minX = joints2D[i][0];
+		if (joints2D[i][0] > maxX) maxX = joints2D[i][0];
+
+		if (joints2D[i][1] < minY) minY = joints2D[i][1];
+		if (joints2D[i][1] > maxY) maxY = joints2D[i][1];
+
+		if (joints2D[i][2] < minZ) minZ = joints2D[i][2];
+		if (joints2D[i][2] > maxZ) maxZ = joints2D[i][2];
+	}
 }
 
 void initDatas()
@@ -78,3 +173,8 @@ void initDatas()
 		posData[i] = new double[POS_JOINT_DATA_NUM];
 	}
 }
+
+/*
+jointPos.x = 156.8584456124928 + 0.0976862095248 * data[i][9] - 0.0006444357104 * data[i][10] + 0.0015715946682 * data[i][11];
+jointPos.y = 125.5357201011431 + 0.0002153447766 * data[i][9] - 0.1184874093530 * data[i][10] - 0.0022134485957 * data[i][11];
+*/
